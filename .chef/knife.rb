12b18802fc0692227cb5f4b635bceca2784ad2d1
Tai Mai <taimai@ooyala.com>
2014-12-05 16:45:48 -0800

    Filled in info

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -1,1 +1,14 @@
-cookbook_path [ '/Users/taimai/dev/chef/firststeps/chef-repo/cookbooks' ]
+current_dir = File.dirname(__FILE__)
+log_level                :info
+log_location             STDOUT
+node_name                "taimai"
+client_key               "#{current_dir}/taimai.pem"
+validation_client_name   "tainyhome-validator"
+validation_key           "#{current_dir}/tainyhome-validator.pem"
+chef_server_url          "https://api.opscode.com/organizations/tainyhome"
+cache_type               'BasicFile'
+cache_options( :path => "#{ENV['HOME']}/.chef/checksums" )
+cookbook_path            ["#{current_dir}/../cookbooks"]
+cookbook_copyright       "tainy company"
+cookbook_license         "apachev2"
+cookbook_email           "taimai@hotmail.com"
